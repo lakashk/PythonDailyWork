b6abe37e39564fa33861b608abebf59ad7baa484
Akash Lokhande <113714518+lakashk@users.noreply.github.com>
2023-02-17 16:02:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,181 +1,205 @@
-# a ,b ,c = input("Enter 3 string\n").split(" ")
-# print(a,b,c)
-
-
-# i= 123
-# s =str(i)
-# print(s)
-# print(s[int(len(s))-1])
-
-# sample = "Hello there"
-#
-# #print(sample.title())
-#
-# print(sample + sample)
-# #print(sample - sample)
-# print(sample * 3)
-# # print(sample - 3) #error
-
-
-# i = int(input("Enter value"))
-# i = str(i)
-# last_d = i[len(i)-1]
-# first_d = i[0]
-# add = int(last_d) + int(first_d)
-# print(add)
-
-# #reversre string
-# s= "This is string"
-# print(s[::-1])
-# print(s.split(" ")[::-1])
-
-
-# i = "ABCDEF"
-# # print(i[5:3])
-# print(i[5:1:-1])
-# print(i[5:1:-1])
-# print(i[-2:-4:-1])
-# # print(i[len(i)])
-# print(i[-6])
+# #hackerrank Q rangoli
+# cstr = "ABCD"
+# print("The Centre aligned string is :")
+# print(cstr.center(10,'-'))
+# print(cstr.ljust(10,'-'))
+# print(cstr.rjust(10,'-'))
 #
 #
-# i = [0,1,2,3,4,5,6,7]
-# print(i[0:6])
-# print(i[4:6:2])
-# print(i[0:6:4])
-# print(i[0:6:2])
+# #Hackerrank  Q string formating
+# i = 17
+# s='1     1     1     1'
+# for i in range(1,18):
+#     print(str(i),
+#           format(i , "o").rjust(2," "),
+#           format(i , "x").upper().rjust(2," "),
+#           format(i, "b").rjust(2," "))
+import json
+
+# f = ["apple", "banana", "cherry","mango"]
+# new = [i for i in f if "a" in i]
+# print(new)
 #
-# h = "HarryPotter"
-# p = "PeterPan"
-# d = h[0:5]
-# f = p[5::]
-# print(d+f)
-# # print(p[])
-
-
-# str = input("Enter Input")
-# print((str[::2]).upper())
-
-temp = ''
-# for i in range(len(str)):
-#     if i % 2 == 0:
-#         temp = temp + str[i].upper()
-#     else:
-#         temp = temp + str[i].lower()
-# print(temp)
-
-# for i , x in enumerate(str):
-#     print(i , x)
-#     if i % 2 == 0:
-#         temp =temp + x.upper()
-#     else:
-#         temp = temp + x.lower()
-# print(temp)
-
-
-# count number of the vovels
-
-# str = input("Enter Input")
-# # count =0
-# # for i in str:
-# #     if i in ('a','e','i','o','u'):
-# #         count = count + 1
-# # print(count)
+# f = [1,2,3,4,5,6]
+# n = [x for x in f if x > 3]
+# n = [x for x in f if len(str(x)) == 1]
+# print(n)
 #
-# k = str.lower()
-# total = 0
-# total += k.count('a')
-# total += k.count('e')
-# total += k.count('i')
-# total += k.count('o')
-# total += k.count('u')
-# print(total)
 
 
-
-#Triangular number
-# i = int(input("Enter number"))
-# sum=0
-# for x in range(1, i):
-#     sum = sum + x
-#     if sum == i:
-#         print("Triangular")
-#         break
-#     else:
-#         print("Not Triangular")
-
-# sum_array = []
-# sum_n = 0
-# n = int(input("Enter a number:"))
-# for i in range(n):
-#     sum_n = sum_n + i
-#     sum_array.append(sum_n)
-# if n in sum_array:
-#     print("It is a triangular number")
-# else:
-#     print("It's not a triangular number")
-
-
-# def temp_function():
-#     for i in range(0 , 100 , 5):
-#         yield i
-# temp = temp_function()
-# print(temp.__next__())
-# print(temp.__next__())
-# print(temp.__next__())
-
-# def temp_function(range):
-#     for i in range(0 ,5):
-#         print((i+1) * "0")
-
-
-#Palindrome
-# i = [1,2,3,4]
-# #stack -- lifo
 #
-# i.pop(-1)
-# print(i)
-
-# str = input("enter string")
-# temp_list = []
-# for i in str:
-#     temp_list.append(i)
-# reversed_list = temp_list[::-1]
-# print(reversed_list, temp_list)
-# if reversed_list == temp_list:
-#     print("Palindrome")
-# else:
-#     print("Not Palindrome")
-
-# dict = {"name": "Lokhande", "num": 1, "DOB":1998}
-# print(dict["num"])
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 #
 #
-# dict1 = {"name": "Akash", "usn": "101", "Location":"Pune"}
-# n = input("Enter text")
-# list = [dict, dict1]
-# for item in list:
-#     if n in item["name"]:
-#         print(True)
-#         print(dict1)
-#     elif n in item["name"]:
-#         print(dict)
+# newlist = [i for i in fruits if "a" in i]
+# print(newlist)
+#
+# student_1 ={
+#     "name": "Cezane",
+#     "USN":"006644",
+#     "locaion": "Nepal"
+# }
+#
+# student_2 ={
+#     "name": "Junee",
+#     "USN":"113322",
+#     "locaion": "Nepal"
+# }
+#
+# student_3 ={
+#     "name": "Anish",
+#     "USN": "1133222",
+#     "locaion": "Nepal"
+# }
+#
+# student_4 ={
+#     "name": "Nisht",
+#     "USN": "1133222",
+#     "locaion": "Nepal"
+# }
+#
+# friend_list = [student_1, student_2, student_3, student_4]
+#
+#
+#
+# student_1 ={
+#     "name": "Cezane",
+#     "USN":"006644",
+#     "locaion": "Nepal"
+# }
+#
+# student_2 ={
+#     "name": "Junee",
+#     "USN":"113322",
+#     "locaion": "Nepal"
+# }
+#
+# student_3 ={
+#     "name": "Anish",
+#     "USN": "1133222",
+#     "locaion": "Nepal"
+# }
+#
+# student_4 ={
+#     "name": "Nisht",
+#     "USN": "1133222",
+#     "locaion": "India"}
+# friend_list = [student_1, student_2, student_3, student_4]
+#
+# # my_list = [x for x in fruits if len(str(x)) == 1]
+# # print(my_list)
+#
+# print(friend_list)
+#
+#
+# my_nepal = [x for x in friend_list if x["locaion"]=="Nepal"]
+# print(my_nepal)
+#
+# my_india = [x for x in friend_list if x["locaion"]=="India"]
+# print(my_india)
+#
+# usn = [x for x in friend_list if x["USN"].count("3")>=1 ]
+# print(usn)
+#
+# name = [x for x in friend_list if x["name"].count("ee")>=1]
+# print(name)
+#
+#
 
-dict = {"name": "Lokhande", "num": 1, "DOB": 1998}
-dict1 = {"name": "Akash", "usn": "101", "Location":"Pune"}
-dict2 = {"name": "nsdjkd", "usn": "101", "Location":"Pune"}
 
-temp = [dict,dict2,dict1]
+## install request package in python
+#get
+#post
+#delete
+#update
 
-sh= input("enter value")
-sh=sh.lower()
+# import requests
+# request_url = "https://jsonplaceholder.typicode.com/todos/1"
+#
+# respose = requests.get(url = request_url)
+# print(respose)
+# print(respose.text)
+# print(respose.json())
 
-for i in temp:
-    if i["name"].count(sh) >=1:
-        print(i)
-        break
 
-    if i["usn"].count(sh) >=1:
-        print(i)
-        break
 
+
+# req_url = "https://jsonplaceholder.typicode.com/posts"
+#
+# res = requests.get(url= req_url)
+# print(res)
+# # ls = [i for i in res if i["title"] <= 8]
+# # print(ls)
+#
+# for data in res.json():
+#     if len(data.get("title").split()) <= 8:
+#         print(data)
+
+
+# def sample_function(a,*args):
+#     if (a==10):
+
+# from url take all email id and store in a list
+# import requests
+# res_url = "https://jsonplaceholder.typicode.com/comments"
+# res = requests.get(url= res_url,  params={
+#                    "postId":4})
+# print(res.json())
+# list =[]
+# for i in res.json():
+#     list.append(i["email"])
+#     print(list)
+#
+#
+
+# wap to print op where e is > 2 times in names in post5
+# import requests
+# res_url = "https://jsonplaceholder.typicode.com/comments"
+# res = requests.get(url=res_url, params={"postId" : "5"})
+# op = [x for x in res.json() if x["name"].count('e') > 2]
+# print(op)
+
+
+#create resource --post
+# import requests
+# res_url = "https://jsonplaceholder.typicode.com/posts"
+# sample_entry ={
+#     "title":'voldormort',
+#     "body": 'bar',
+#     "userId": 1
+# }
+# string_sample = json.dumps(sample_entry)
+# res = requests.post(url=res_url, data=string_sample , headers={
+#     'content-type': 'application/json ; charset=UTF-8'
+# })
+# print(res)
+# print(res.json())
+
+
+#wap to print random op of 5
+# import requests
+# import json
+# req_url = "https://api.publicapis.org/random"
+# l=[]
+# for i in range(5):
+#     res = requests.get(url=req_url)
+#     l.append(res.json())
+#     print(l)
+#
+#
+
+import requests
+import json
+# req_url = "https://jsonplaceholder.typicode.com/comments?postId=1"
+# user_input = input("Please enter the postid\n")
+# user_input_split = user_input.split(":")
+# sample_param ={}
+# sample_param[user_input_split[0]] = user_input_split[1]
+# res = requests.get(req_url,params=sample_param)
+# print(res.json())
+
+
+request_url = "https://jsonplaceholder.typicode.com/comments"
+response = requests.get(url=request_url,params={"postId": input("")})
+va = (response.json())
+print(va)
