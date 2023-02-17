4c54a765af68694de537e821f355dfef0622efbb
Akash Lokhande <113714518+lakashk@users.noreply.github.com>
2023-02-17 16:01:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,161 +1,181 @@
-# import operator
+# a ,b ,c = input("Enter 3 string\n").split(" ")
+# print(a,b,c)
+
+
+# i= 123
+# s =str(i)
+# print(s)
+# print(s[int(len(s))-1])
+
+# sample = "Hello there"
 #
-# i = 5
-# j = 2
+# #print(sample.title())
 #
-# print(operator.floordiv(i, j))
-# print(operator.pow(i ,j))
+# print(sample + sample)
+# #print(sample - sample)
+# print(sample * 3)
+# # print(sample - 3) #error
 
 
-#take input in celcius and convert into farenheight
-# import operator
-# c = int(input("Enter value"))
-# f= operator.add(operator.mul(c,1.8),32)
-# print(f)
+# i = int(input("Enter value"))
+# i = str(i)
+# last_d = i[len(i)-1]
+# first_d = i[0]
+# add = int(last_d) + int(first_d)
+# print(add)
+
+# #reversre string
+# s= "This is string"
+# print(s[::-1])
+# print(s.split(" ")[::-1])
 
 
-# i = int(input("Enter num"))
-# # num = i.split()
-# str=str(i)
-# for x in i:
-#     if int(x[0]) + int(x[1]) == int(x[2]):
-#         print(True)
-
-# i = int(input("Enter num"))
-# sum= int(i[0]) + int(i[1])
-# print(sum)
-
-
-
-# num = input("enter a 3 digit number:")
-# sum = int(num[0]) + int(num[1])
-# if sum == int(num[2]):
-#  print(f"{num} is a handsome number")
-# else:
-#     print(f"{num} is not a handsome number")
-
-
-
-
-# import math
-# i = int(input("Enter num"))
-# if (i>99 and i<999):
-#     l_index = i % 10
-#     i = math.floor(i/10)
+# i = "ABCDEF"
+# # print(i[5:3])
+# print(i[5:1:-1])
+# print(i[5:1:-1])
+# print(i[-2:-4:-1])
+# # print(i[len(i)])
+# print(i[-6])
 #
-#     m_index = i % 10
-#     i = math.floor(i / 10)
 #
-#     f_index = i % 10
+# i = [0,1,2,3,4,5,6,7]
+# print(i[0:6])
+# print(i[4:6:2])
+# print(i[0:6:4])
+# print(i[0:6:2])
 #
-#     if ((f_index + m_index) == l_index):
-#         print("hansome number")
+# h = "HarryPotter"
+# p = "PeterPan"
+# d = h[0:5]
+# f = p[5::]
+# print(d+f)
+# # print(p[])
+
+
+# str = input("Enter Input")
+# print((str[::2]).upper())
+
+temp = ''
+# for i in range(len(str)):
+#     if i % 2 == 0:
+#         temp = temp + str[i].upper()
 #     else:
-#         print("not hansome number")
+#         temp = temp + str[i].lower()
+# print(temp)
+
+# for i , x in enumerate(str):
+#     print(i , x)
+#     if i % 2 == 0:
+#         temp =temp + x.upper()
+#     else:
+#         temp = temp + x.lower()
+# print(temp)
 
 
+# count number of the vovels
 
-# import operator
+# str = input("Enter Input")
+# # count =0
+# # for i in str:
+# #     if i in ('a','e','i','o','u'):
+# #         count = count + 1
+# # print(count)
 #
-# a = int(input("please enter three digits number: "))
-#
-# res = [int(x) for x in str(a)]
-#
-# print(operator.add(res[0],res[-1]))
+# k = str.lower()
+# total = 0
+# total += k.count('a')
+# total += k.count('e')
+# total += k.count('i')
+# total += k.count('o')
+# total += k.count('u')
+# print(total)
 
 
 
-
-# def fun(a, *args):
-#     for arg in args:
-#         print(arg)
-#
-# def fun(a, *args):
-#     print("HEllo")
-#
-# fun(123,1233,1212,12,1)
-
-
-# a = "abcbed"
-# sum = 0
-# for i in a :
-#     sum +=ord(i)
-# print(sum)
-
-
-# i = 'AkashLokhande'
+#Triangular number
+# i = int(input("Enter number"))
 # sum=0
-# for x in i:
-#     sum +=ord(x)
-# print(sum)
+# for x in range(1, i):
+#     sum = sum + x
+#     if sum == i:
+#         print("Triangular")
+#         break
+#     else:
+#         print("Not Triangular")
 
-# n= int(input("Enter value: "))
-# sum = 0
-# for x in range(1, n):
-#     if(n % x == 0):
-#         sum = sum + x
-# if (sum == n):
-#     print("Number is The Perfect number")
+# sum_array = []
+# sum_n = 0
+# n = int(input("Enter a number:"))
+# for i in range(n):
+#     sum_n = sum_n + i
+#     sum_array.append(sum_n)
+# if n in sum_array:
+#     print("It is a triangular number")
 # else:
-#     print("Number is not perfect number")
+#     print("It's not a triangular number")
 
-# import math
-# def quad(*args):
-#     print("Permeter: ",sum(args))
-#     print("Area: ",math.prod(args))
-#     d =
+
+# def temp_function():
+#     for i in range(0 , 100 , 5):
+#         yield i
+# temp = temp_function()
+# print(temp.__next__())
+# print(temp.__next__())
+# print(temp.__next__())
+
+# def temp_function(range):
+#     for i in range(0 ,5):
+#         print((i+1) * "0")
+
+
+#Palindrome
+# i = [1,2,3,4]
+# #stack -- lifo
 #
-# a=10
-# b=20
-# c=30
-# d=40
-# quad(a,b,c,d)
+# i.pop(-1)
+# print(i)
 
+# str = input("enter string")
+# temp_list = []
+# for i in str:
+#     temp_list.append(i)
+# reversed_list = temp_list[::-1]
+# print(reversed_list, temp_list)
+# if reversed_list == temp_list:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
 
-
-# import math
-# def calc(*args):
-#  print("Perimeter ", sum(args))
-#  print("Area", math.prod(args))
-# p = math.sqrt(((args[0] * args[2])+(args[1] * args[3]))*((args[0] * args[3])+(args[1] * args[2]))/ ((args[0] * args[1])+(args[2] * args[3])))
-# q = math.sqrt(((args[0] * args[2])+(args[1] * args[3]))*((args[0] * args[1])+(args[2] * args[3]))/ ((args[0] * args[3])+(args[1] * args[2])))
-# return [p, q]
-# diagonals = calc(1,2,3,4)
-# print(f"daigonal is {round(diagonals[0], 2)}, {round(diagonals[1], 2)}")
-
-
-
-# def hello(u):
-#     for i in u:
-#         yield i * i
-# print(list(hello([1,2,3,4,5,6])))
-
-
-# i= [12344556545]
-# d={}
-# for x in i:
-#     d[items]= i.count(items)
+# dict = {"name": "Lokhande", "num": 1, "DOB":1998}
+# print(dict["num"])
 #
-# for key , value in d.items():
-#     print("%d : %d"%(key , value))
-# print(d)
+#
+# dict1 = {"name": "Akash", "usn": "101", "Location":"Pune"}
+# n = input("Enter text")
+# list = [dict, dict1]
+# for item in list:
+#     if n in item["name"]:
+#         print(True)
+#         print(dict1)
+#     elif n in item["name"]:
+#         print(dict)
 
+dict = {"name": "Lokhande", "num": 1, "DOB": 1998}
+dict1 = {"name": "Akash", "usn": "101", "Location":"Pune"}
+dict2 = {"name": "nsdjkd", "usn": "101", "Location":"Pune"}
 
-# i = int(input("Please enter numbers : "))
-# l = []
-# for j in str(i):
-# l.append(j)
-# l2= set(l)
-# print(l)
-# print(l2)
-# for s in l2:
-# print(f'{s}={l.count(s)}')
+temp = [dict,dict2,dict1]
 
+sh= input("enter value")
+sh=sh.lower()
 
+for i in temp:
+    if i["name"].count(sh) >=1:
+        print(i)
+        break
 
-from math import sqrt
-def solve(n):
-   sq_root = int(sqrt(n))
-   return (sq_root*sq_root) == n
-n = 36
-print (solve(n))
+    if i["usn"].count(sh) >=1:
+        print(i)
+        break
+
